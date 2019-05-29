14db16ea15d8bc9ad987ce27ba9a7f37efb8564b
Alfred Reynolds <alfred@mazuma.net.au>
2019-05-28 19:33:08 -0700

    - move the custom scroll JS into its own file as the gcloud instance appears to be stripping it

---
+++ server/assets/static/catalog.js
@@ -0,0 +1,20 @@
+      // offset any #id scroll to account for the nav bar
+      // The function actually applying the offset
+      function offsetAnchor() {
+        if (location.hash.length !== 0) {
+          window.scrollTo(window.scrollX, window.scrollY - 107);
+        }
+      }
+
+      // Captures click events of all <a> elements with href starting with #
+      $(document).on('click', 'a[href^="#"]', function (event) {
+        // Click events are captured before hashchanges. Timeout
+        // causes offsetAnchor to be called after the page jump.
+        window.setTimeout(function () {
+          offsetAnchor();
+        }, 0);
+      });
+
+      // Set the offset when entering page with hash present in the url
+      window.setTimeout(offsetAnchor, 0);
+      
